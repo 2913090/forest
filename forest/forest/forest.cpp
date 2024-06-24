ddfc60f81f59c9df6667a80d5a5092bad3491730
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-06-24 14:54:46 +0300

    Add files via upload

---
+++ forest/forest/forest.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Tree {
+private:
+    int id;
+    int age;
+    float key;
+protected:
+    static int curId;
+public:
+    ~Tree() = default;
+};
+
+class Oak : public Tree {
+private:
+    int id;
+    int age = 1;
+    float key = 2;
+public:
+    Oak() :id(curId++) {}
+};
+
+class Birch : public Tree {
+private:
+    int id;
+    int age = 1;
+    float key = 3;
+public:
+
+};
+
+class Spruce : public Tree {
+private:
+    int id;
+    int age = 1;
+    float key = 2.5;
+public:
+
+};
+
+class Forest {
+private:
+    vector<Tree> trees;
+public:
+
+};
+
+
+int Tree::curId = 1;
+
+int main()
+{
+
+}
